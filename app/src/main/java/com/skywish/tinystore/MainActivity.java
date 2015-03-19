fbf137cdbb072dd92562e12c73398e48bd04b8a4
Skywish <skywish@yeah.net>
2015-03-19 23:10:32 +0800

    add query info

--- app/src/main/java/com/skywish/tinystore/MainActivity.java
+++ app/src/main/java/com/skywish/tinystore/MainActivity.java
@@ -1,9 +1,81 @@
 package com.skywish.tinystore;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.skywish.tinystore.adapter.MyAdapter;
+import com.skywish.tinystore.bean.Lost;
+
+import java.util.List;
+
+import cn.bmob.v3.BmobQuery;
+import cn.bmob.v3.listener.FindListener;
+
 /**
  * Created by skywish on 2015/3/17.
  */
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements View.OnClickListener{
 
+    private ProgressBar progressBar;
+    private TextView tv_no;
+    private ListView list_lost;
+    private Button btn_add;
+    private MyAdapter lostAdapter;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        progressBar = (ProgressBar) findViewById(R.id.progress);
+        tv_no = (TextView) findViewById(R.id.tv_no);
+        list_lost = (ListView) findViewById(R.id.list_lost);
+        btn_add = (Button) findViewById(R.id.btn_add);
+
+        queryLost();
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    public void queryLost() {
+        BmobQuery<Lost> query = new BmobQuery<Lost>();
+        query.order("-createdAt");      //按照时间降序
+        //执行查询，第一个参数为上下文，第二个参数为查找的回调
+        query.findObjects(this, new FindListener<Lost>() {
+            @Override
+            public void onSuccess(List<Lost> losts) {
+                if (losts == null || losts.size() == 0) {
+                    showErrorView(0);
+                    return;
+                }
+                progressBar.setVisibility(View.GONE);
+                lostAdapter = new MyAdapter(getApplicationContext(),
+                        R.layout.item_list, losts);
+                list_lost.setAdapter(lostAdapter);
+            }
+
+            @Override
+            public void onError(int code, String arg0) {
+                showErrorView(0);
+            }
+        });
+    }
+
+    private void showErrorView(int tag) {
+        progressBar.setVisibility(View.GONE);
+        list_lost.setVisibility(View.GONE);
+        tv_no.setVisibility(View.VISIBLE);
+        if (tag == 0) {
+            tv_no.setText("list_no_data_lost");
+        } else {
+            tv_no.setText("list_no_data_found");
+        }
+    }
 }
---
+++ app/src/main/java/com/skywish/tinystore/adapter/MyAdapter.java
@@ -0,0 +1,57 @@
+package com.skywish.tinystore.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.skywish.tinystore.R;
+import com.skywish.tinystore.bean.Lost;
+
+import java.util.List;
+
+/**
+ * Created by skywish on 2015/3/18.
+ */
+public class MyAdapter extends ArrayAdapter<Lost> {
+
+    private int resourceId;
+
+    public MyAdapter(Context context, int textViewResourceId, List<Lost> objects) {
+        super(context, textViewResourceId, objects);
+        resourceId = textViewResourceId;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Lost lost = getItem(position);
+        View view;
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            view = LayoutInflater.from(getContext()).inflate(resourceId, null);
+            viewHolder = new ViewHolder();
+            viewHolder.title = (TextView) view.findViewById(R.id.tv_title);
+            viewHolder.describe = (TextView) view.findViewById(R.id.tv_describe);
+            viewHolder.time = (TextView) view.findViewById(R.id.tv_time);
+            viewHolder.phone = (TextView) view.findViewById(R.id.tv_phone);
+            view.setTag(viewHolder);
+        } else {
+            view = convertView;
+            viewHolder = (ViewHolder) view.getTag();
+        }
+        viewHolder.describe.setText(lost.getDescribe());
+        viewHolder.phone.setText(lost.getPhone());
+        viewHolder.title.setText(lost.getTitle());
+        viewHolder.time.setText(lost.getCreatedAt());
+        return view;
+    }
+
+    class ViewHolder {
+        TextView describe;
+        TextView phone;
+        TextView title;
+        TextView time;
+    }
+}
